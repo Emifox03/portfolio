6217820d3f9b087e022bf6fa14867ce625dc4964
Emifox03 <koala032003@hotmail.com>
2022-10-24 13:32:57 +0200

    sdcsd

--- projectp/portfolio/portfolio.js
+++ projectp/portfolio/portfolio.js
@@ -1,6 +1,16 @@
 
 
-function abrirportfolio()
+function abrirportfolioA()
+{
+    window.open("pemilio/main.html","_self");
+}
+
+function abrirportfolioE()
 {
     window.open("pangela/pangela.html","_self");
+}
+
+function abrirportfolioG()
+{
+    window.open("pgomi/main.html","_self");
 }

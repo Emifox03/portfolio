7d1eb925d49188279f171c06e928c2014a7fb150
angeladela <101990989+angeladela@users.noreply.github.com>
2022-10-27 09:57:27 +0200

    jeej

--- projectp/portfolio/pangela/pangela.js
+++ projectp/portfolio/pangela/pangela.js
@@ -2,3 +2,18 @@ Window.alert(Panoli);
 
 const canvas=document.getElementById('game'); 
 const ctx=canvas.getContext('2d');
+
+
+function drawGame() {
+    clearScreen();
+    
+}
+
+
+function clearScreen() {
+    ctx.fillStyle= 'aquamarine'
+    ctx.fillRect(0,0,canvas.clientWidth,canvas.clientHeight)
+    
+}
+
+drawGame();

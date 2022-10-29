9e25ebd4d70e8f4969f423a9ba25282731fc0850
Emifox03 <koala032003@hotmail.com>
2022-10-29 21:56:54 +0200

    akjsbkjacb

--- projectp/portfolio/pemilio/script.js
+++ projectp/portfolio/pemilio/script.js
@@ -1,4 +1,7 @@
+var setScroll = 0;
+
 function myFunction() {
+    setScroll = 0;
     document.getElementById("cuadraoterminal").innerHTML =  `<div class="BordeTerminal">
                                                                 <h6 class="TituloTerminal">Terminal</h6>
                                                             </div>
@@ -11,9 +14,16 @@ function myFunction() {
                                                                 <br>
                                                                 <p class="UsuarioTerminal2">Estudios: Ingenieria Informatica UCO</p>
                                                                 <br>
-                                                                <p class="UsuarioTerminal2">Estudios: Has terminado la carrera?(y/n)</p>
+                                                                <p class="UsuarioTerminal2">Quieres saber mas?(Presiona Enter)</p>
                                                                 <p class="GuionTerminal">_</p>
+                                                                <p class="Editable" contenteditable="true"></p>
                                                             </div>`
 
 }
 
+
+document.addEventListener('keydown', function(event) {
+    if(event.keyCode == 13 && setScroll == 0) {
+        document.getElementById("scroll1").scrollIntoView({behavior: "smooth", block: "start", inline: "nearest"});
+    }
+});
